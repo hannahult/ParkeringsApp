1532dec913c6da82c76ab8d58bc4e433b328b5ed
Sabina Karlsson <karlsson.sst@gmail.com>
2024-10-31 13:24:59 +0100

    Det kommer ett random fordon och man får fylla i uppgifter

--- ParkeringsAppLunchTrion/Program.cs
+++ ParkeringsAppLunchTrion/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace ParkeringsAppLunchTrion
+﻿using System.Windows.Markup;
+
+namespace ParkeringsAppLunchTrion
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ange registreringsnummer: ");
+            string regNumber = Console.ReadLine();
+
+            Console.WriteLine("Ange färg på ditt fordon: ");
+            string vehicleColor = Console.ReadLine();
+
+            int randomVehicle = Random.Shared.Next(0, 3);
+
+            switch(randomVehicle)
+            {
+                case 0: //Bil
+                    Console.WriteLine("Är det en elbil?");
+                    Console.WriteLine("[1] ja");
+                    Console.WriteLine("[2] nej");
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    switch (key.KeyChar)
+                    {
+                        case '1':
+                            new Car(regNumber, vehicleColor, true);
+                            break;
+
+                        case '2':
+                            new Car(regNumber, vehicleColor, false);
+                            break;
+                        
+                    }
+                    
+                    break;
+
+                case 1: //Buss
+                    Console.WriteLine("Hur många platser är det i bussen?");
+                    Int32.TryParse(Console.ReadLine(), out int numberOfSeats);
+
+                    new Bus(regNumber, vehicleColor, numberOfSeats);
+                    break;
+
+                case 2: //MC
+                    Console.WriteLine("Vilket märke är det på motorcykeln?");
+                    string mcBrand = Console.ReadLine();
+
+                    new MC(regNumber, vehicleColor, mcBrand);
+                    break;
+            }
+
+            
+
+
         }
     }
 }
