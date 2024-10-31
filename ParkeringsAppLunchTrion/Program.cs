b95078d01a282e8e7a40e9daa9eed45e28fa2b6f
Sabina Karlsson <karlsson.sst@gmail.com>
2024-10-31 14:31:04 +0100

    Glömde skapa en ny branch innan vi började med detta
    La till fordon på en parkeringsplats

--- ParkeringsAppLunchTrion/Program.cs
+++ ParkeringsAppLunchTrion/Program.cs
@@ -6,52 +6,100 @@ namespace ParkeringsAppLunchTrion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ange registreringsnummer: ");
-            string regNumber = Console.ReadLine();
+            string[] parkingLots = new string[25];
 
-            Console.WriteLine("Ange färg på ditt fordon: ");
-            string vehicleColor = Console.ReadLine();
 
-            int randomVehicle = Random.Shared.Next(0, 3);
-
-            switch(randomVehicle)
+            while (true)
             {
-                case 0: //Bil
-                    Console.WriteLine("Är det en elbil?");
-                    Console.WriteLine("[1] ja");
-                    Console.WriteLine("[2] nej");
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    switch (key.KeyChar)
-                    {
-                        case '1':
-                            new Car(regNumber, vehicleColor, true);
-                            break;
 
-                        case '2':
-                            new Car(regNumber, vehicleColor, false);
-                            break;
-                        
-                    }
-                    
-                    break;
+                Console.WriteLine("Ange registreringsnummer: ");
+                string regNumber = Console.ReadLine();
 
-                case 1: //Buss
-                    Console.WriteLine("Hur många platser är det i bussen?");
-                    Int32.TryParse(Console.ReadLine(), out int numberOfSeats);
+                Console.WriteLine("Ange färg på ditt fordon: ");
+                string vehicleColor = Console.ReadLine();
 
-                    new Bus(regNumber, vehicleColor, numberOfSeats);
-                    break;
+                int randomVehicle = Random.Shared.Next(0, 3);
 
-                case 2: //MC
-                    Console.WriteLine("Vilket märke är det på motorcykeln?");
-                    string mcBrand = Console.ReadLine();
+                switch (randomVehicle)
+                {
+                    case 0: //Bil
+                        Console.WriteLine("Är det en elbil?");
+                        Console.WriteLine("[1] ja");
+                        Console.WriteLine("[2] nej");
+                        ConsoleKeyInfo key = Console.ReadKey();
+                        Console.Clear();
+                        switch (key.KeyChar)
+                        {
+                            case '1':
+                                Car car1 = new Car(regNumber, vehicleColor, true);
+                                for (int i = 0; i < parkingLots.Length; i++)
+                                {
+                                    if (parkingLots[i] == null)
+                                    {
+                                        parkingLots[i] = "Plats " + (i + 1) + "\t\t" + car1.GetType().Name + "\t" + regNumber + "\t" + vehicleColor + "\t" + "Elbil" + "\t" + "sekunder kvar";
+                                        Console.WriteLine(parkingLots[i]);
+                                        break;
+                                    }
+                                }
+                                break;
 
-                    new MC(regNumber, vehicleColor, mcBrand);
-                    break;
+                            case '2':
+                                Car car2 = new Car(regNumber, vehicleColor, false);
+                                for (int i = 0; i < parkingLots.Length; i++)
+                                {
+                                    if (parkingLots[i] == null)
+                                    {
+
+                                        parkingLots[i] = "Plats " + (i + 1) + "\t\t" + car2.GetType().Name + "\t" + regNumber + "\t" + vehicleColor + "\t" + "Bil" + "\t" + "sekunder kvar";
+                                        Console.WriteLine(parkingLots[i]);
+
+                                        break;
+                                    }
+                                }
+                                break;
+
+                        }
+
+                        break;
+
+                    case 1: //Buss
+                        Console.WriteLine("Hur många platser är det i bussen?");
+                        bool lyckad = Int32.TryParse(Console.ReadLine(), out int numberOfSeats);
+                        if (!lyckad)
+                        {
+                            Console.WriteLine("Du har inte angett antal med siffor");
+                        }
+                        Bus bus1 = new Bus(regNumber, vehicleColor, numberOfSeats);
+                        for (int i = 0; i < parkingLots.Length; i++)
+                        {
+                            if (parkingLots[i] == null)
+                            {
+                                parkingLots[i] = "Plats " + (i + 1) + "\t\t" + bus1.GetType().Name + "\t" + regNumber + "\t" + vehicleColor + "\t" + "Antal platser: " + numberOfSeats + "\t" + "sekunder kvar";
+                                Console.WriteLine(parkingLots[i]);
+                                break;
+                            }
+                        }
+                        break;
+
+                    case 2: //MC
+                        Console.WriteLine("Vilket märke är det på motorcykeln?");
+                        string mcBrand = Console.ReadLine();
+
+                        MC mc1 = new MC(regNumber, vehicleColor, mcBrand);
+                        for (int i = 0; i < parkingLots.Length; i++)
+                        {
+                            if (parkingLots[i] == null)
+                            {
+                                parkingLots[i] = "Plats " + (i + 1) + "\t\t" + mc1.GetType().Name + "\t" + regNumber + "\t" + vehicleColor + "\t" + mcBrand + "\t" + " sekunder kvar";
+                                Console.WriteLine(parkingLots[i]);
+                                break;
+                            }
+                        }
+                        break;
+                }
+
+
             }
-
-            
-
 
         }
     }
